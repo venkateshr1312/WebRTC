3a442298e4e1876564e761cc788a405827c21a73
venkateshr1312 <88719163+venkateshr1312@users.noreply.github.com>
2025-03-13 15:49:43 +0530

    Update App.tsx

--- web_meet/src/App.tsx
+++ web_meet/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EncryptionComponent from './components/EncryptionComponent';
-import Meet from './components/Meet';
+import ScreenShare from './components/ScreenShare';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        {/* Define route with dynamic 'id' parameter */}
-        <Route path="/:id" element={<EncryptionComponent />} />
-        <Route path="meet/:type/:phone" element={<Meet />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* Define route with dynamic 'id' parameter */}
+                <Route path="/:id" element={<EncryptionComponent />} />
+                <Route path="meet/:type/:phone" element={<ScreenShare />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
